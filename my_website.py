227c96a083943d8c41f52cd1f0f6cced47bdd19b
System Administrator <root@ITIN000150-MAC.local>
2019-03-13 02:57:24 +0530

    include the function to handle submit

--- my_website.py
+++ my_website.py
@@ -1,10 +1,24 @@
-from flask import Flask,render_template
-app = Flask(__name__)
+from flask import Flask,render_template,request
+import logging
 
+log_file='/var/log/mywebsite.log'
+logging.basicConfig(filename=log_file,format='%(asctime)s-%(message)s', level=logging.CRITICAL)
+app = Flask(__name__)
+counter=0
 
 @app.route('/')
-def hello():
-    return render_template('index.html')
+def index():
+	global counter
+	counter=counter+1
+	logging.critical('No of visit: {0}'.format(counter))
+	return render_template('index.html')
+
+
+@app.route('/submit', methods=['POST','GET'])
+def submit():
+	logging.critical('Message Received: From={0} email={1} Message={2}'.format(request.form['Name'],request.form['_replyto'],request.form['message']))
+	return render_template('thankyou.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=80)
