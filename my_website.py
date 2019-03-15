37e83d35e4d3864985d09cb4e8b1f3384c9a9935
root <root@ip-172-31-94-43.ec2.internal>
2019-03-15 16:40:11 +0000

    added welcome page to the website

--- my_website.py
+++ my_website.py
@@ -5,15 +5,12 @@ import pytz
 from occasions import occasion
 import datetime
 from datetime import date, timedelta
-from tzwhere import tzwhere
 from parameter_store import *
 
 log_file='/var/log/mywebsite.log'
-logging.basicConfig(filename=log_file,format='%(asctime)s-%(message)s', level=logging.INFO)
+logging.basicConfig(filename=log_file,format='%(asctime)s-%(message)s', level=logging.WARNING)
 app = Flask(__name__)
-YEAR='2019'
-counter=0
-
+count=0
 def get_location(ip):
 	url=ipstack_url+'/'+ip+'?access_key='+ipstack_access_key
 	try:
@@ -30,18 +27,22 @@ def get_location(ip):
 		country_code='IN'
 		latitude='12.9833'
 		longitude='77.5833'
-	return city,country_code,latitude,longitude
+	if not city:
+		city='Bengaluru'
+		country_code='IN'
+		latitude='12.9833'
+		longitude='77.5833'
+	return city.encode('utf-8'),country_code.encode('utf-8'),str(latitude),str(longitude)
 
-def get_time_date(latitude,longitude):
-	tz = tzwhere.tzwhere()
-	timezone_str = tz.tzNameAt(float(latitude),float(longitude))
-	zone = pytz.timezone(timezone_str)
-	now = datetime.datetime.now(zone)
-	hour=now.strftime("%H")
-	date=now.strftime("%d")
-	month=now.strftime("%B")[:3]
-	month_num=now.strftime("%m")
-	return hour,date,month,month_num
+def get_time_date(time_date):
+	date=time_date.split(' ')[0]
+	time=time_date.split(' ')[1]
+	year=date.split('-')[0]
+	month=date.split('-')[1]
+	day=date.split('-')[2]
+	hour=time.split(':')[0]
+	month_name='Mar'
+	return year,month,day,hour,month_name
 
 def get_wishing(hour):
 	if int(hour)<12:
@@ -61,7 +62,7 @@ def get_greeting(date,month):
 	return greeting
 
 
-def get_weather(city):
+def get_city_details(city):
 	url=apixu_url+city
 	try:
 		resp=requests.get(url)
@@ -70,6 +71,7 @@ def get_weather(city):
 		max_temp=data['forecast'][u'forecastday'][0]['day']['maxtemp_c']
 		min_temp=data['forecast'][u'forecastday'][0]['day']['mintemp_c']
 		forcast=data['forecast'][u'forecastday'][0]['day']['condition']['text']
+		time_date=data['location']['localtime']
 	except Exception as e:
 		logging.error('got below error:')
 		logging.error(e)
@@ -77,18 +79,17 @@ def get_weather(city):
 		max_temp='27'
 		min_temp='18'
 		forcast='Pleasent'
-	return temp,max_temp,min_temp,forcast
+		time_date='2019-03-15 15:53'
+	return temp,max_temp,min_temp,forcast,time_date
 
-def get_news(city,month_num):
+def get_news(city,month_num,year):
 	headlines=[]
 	day = date.today() - timedelta(1)
-	yesterday='{0}-{1}-{2}'.format(YEAR,month_num,day.strftime('%d'))
+	yesterday='{0}-{1}-{2}'.format(year,month_num,day.strftime('%d'))
 	url=newsapi_url+city+'&from='+yesterday
-	logging.error(url)
 	try:
 		resp=requests.get(url)
 		data=resp.json()
-		logging.error(data)
 		for x in range(0,5):
 			headlines.append('. '+data['articles'][x]['title']+'.')
 	except Exception as e:
@@ -99,21 +100,18 @@ def get_news(city,month_num):
 
 @app.route('/')
 def welcome():
-	global counter
-	counter=counter+1
-	
+        global count
+        count=count+1
 	ip=request.remote_addr
-	if str(ip)=='127.0.0.1':
-		ip='122.167.29.17'
-	
-	logging.critical('[{0}]Total number of visit: {1}'.format(ip,counter))
 	city,country_code,latitude,longitude=get_location(ip)
-	hour,date,month,month_num=get_time_date(latitude,longitude)
+	temp,max_temp,min_temp,forcast,time_date=get_city_details(city)
+	year,month,day,hour,month_name=get_time_date(time_date)
 	wishing=get_wishing(hour)
-	greeting=get_greeting(date,month)
-	temp,max_temp,min_temp,forcast=get_weather(city)
-	headlines=get_news(city,month_num)
+	greeting=get_greeting(day,month)
+	headlines=get_news(city,month_name,year)
 	welcome_msg=wishing+' and '+greeting
+        logging.warning('A person visited from %s'%city)
+        logging.warning('Total number of visit: %s'%count)
 	return render_template('welcome.html',welcome_msg=welcome_msg,city=city,temp=temp,max_temp=max_temp,min_temp=min_temp,forcast=forcast,headlines=headlines)
 
 @app.route('/profile')
@@ -128,4 +126,5 @@ def submit():
 
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0',port=80)
+    app.run(threaded=True,debug=True,host='0.0.0.0',port=80)
+
